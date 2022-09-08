579c9436774e3bd8683ac196d0761dfc53caa725
luizsantos-1 <64617480+luizsantos-1@users.noreply.github.com>
2022-09-08 14:48:39 -0300

    Update A2 - entrega

--- projeto_1/A2/design.vhd
+++ projeto_1/A2/design.vhd
@@ -1,3 +1,8 @@
+library IEEE;
+use IEEE.numeric_bit.all;
+use STD.textio.all;
+
+
 entity rom_arquivo is 
 	port (
     	addr: in bit_vector (4 downto 0);
@@ -8,10 +13,41 @@ end rom_arquivo;
 
 architecture rom_arquivo_arch of rom_arquivo is 
 	-- declaracoes 
-    type mem_rom is array (0 to 31) of bit_vector (7 downto 0);
-    signal memoria: mem_rom := init_mem("conteudo_rom_ativ_02_carga.dat");  
+    constant MEM_DEPTH : integer := 2**5;
+	type mem_type is array (0 to 31) of bit_vector(7 downto 0);
+    
+    signal retorno: mem_type;
+
+impure function init_mem(mif_file_name : in string) return mem_type is
+    file mif_file : text open read_mode is mif_file_name;
+    variable mif_line : line;
+    variable temp_bv : bit_vector(7 downto 0);
+    variable temp_mem : mem_type;
+    
+    
+begin
+    for i in mem_type'range loop
+        readline(mif_file, mif_line);
+        read(mif_line, temp_bv);
+        temp_mem(i) := temp_bv;
+    end loop;
+    
+    return temp_mem;
+end function;
+
+
+    signal memoria: mem_type := init_mem("conteudo_rom_ativ_02_carga.dat");  
+    
+    
+    
 begin 
 
     data <= memoria(to_integer(unsigned(addr))); 
     		
 end rom_arquivo_arch;
+
+
+
+
+
+
--- projeto_1/A2/testbench.vhd
+++ projeto_1/A2/testbench.vhd
@@ -1,2 +1,50 @@
+library IEEE;
+use IEEE.numeric_bit.all;
 
+ 
+entity testbench is
+-- empty
+end testbench; 
+
+architecture tb of testbench is
+
+	component rom_arquivo is
+port (
+    addr: in bit_vector (4 downto 0);
+    data: out bit_vector(7 downto 0)
+    );
+end component;
+
+-- DUT component
+
+signal ende: bit_vector (4 downto 0);
+signal saida: bit_vector (7 downto 0);
+
+
+begin
+  -- Connect DUT
+  DUT: rom_arquivo port map(ende, saida);
+  
+  process
+  begin
+  
+  	assert false report "Test start." severity note;
+    
+    ende <= "00000";
+    wait for 2 ns;
+    
+    assert(saida ="00000000") report "Fail 0+0" severity error;
+    
+    ende <= "00101";
+    wait for 2 ns;
+    
+    assert(saida ="01010101") report "Fail 0+0" severity error;
+   
+
+    -- Informa fim do teste
+    assert false report "Test done." severity note;
+    wait; -- Interrompe execução
+ 
+  end process;
+end tb;
 
